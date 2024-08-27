384e31b3765e569117d3522282e6306c05c279a0
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-27 13:20:41 -0400

    chore: run bot on edge functions with preferred regions in Europe

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
-
 export const fetchCache = "force-no-store";
+export const runtime = "edge";
+export const preferredRegion = ["fra1", "cdg1", "lhr1"];
 
 import { webhookCallback } from "grammy";
 import { bot } from "~/configs/bot";
