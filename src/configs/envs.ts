f5b10cd3ccfe95353f9d6256d8af189da1b5098e
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-26 21:22:37 -0400

    chore: make port env optional

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -1,9 +1,9 @@
-import { url, object, parse, pipe, string } from "valibot";
+import { url, object, optional, parse, pipe, string } from "valibot";
 
 const envsSchema = object({
 	TELEGRAM_TOKEN: string(),
 	APP_URL: pipe(string(), url()),
-	PORT: string(),
+	PORT: optional(string(), "3000"),
 });
 
 export const envs = parse(envsSchema, process.env);
