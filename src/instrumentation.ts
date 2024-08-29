706fe10ca0a853258f48495ade7890eb678d0311
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-29 18:44:37 -0400

    fix: always set webhook to drop pending updates

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -6,8 +6,6 @@ export async function register() {
 
 		const WEBHOOK_URL = `https://${envs.APP_URL}/api/bot`;
 		try {
-			const { url } = await bot.api.getWebhookInfo();
-			if (url === WEBHOOK_URL) return;
 			await bot.api.setWebhook(WEBHOOK_URL, { drop_pending_updates: true });
 		} catch (err) {
 			console.error("Failed to set webhook", err);
