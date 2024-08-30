aaf782731312f81feb1fcffde7159e5925ac5355
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-30 15:49:24 -0400

    chore: remove useless VERCEL_GIT_PULL_REQUEST_ID env

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -4,7 +4,6 @@ const _envs = cleanEnv(process.env, {
 	ADMIN_CHAT_ID: num({ default: -723672593 }),
 	TELEGRAM_TOKEN: str(),
 	VERCEL_PROJECT_PRODUCTION_URL: str({ default: "mysubdomain.loca.lt" }),
-	VERCEL_GIT_PULL_REQUEST_ID: str({ default: undefined }),
 	PORT: port({ default: 3000 }),
 	DATABASE_URL: str(),
 });
@@ -12,7 +11,6 @@ const _envs = cleanEnv(process.env, {
 export const envs = {
 	..._envs,
 	APP_URL: _envs.VERCEL_PROJECT_PRODUCTION_URL,
-	PULL_REQUEST_ID: _envs.VERCEL_GIT_PULL_REQUEST_ID,
 	isDevelopment: _envs.isDevelopment,
 	isProduction: _envs.isProduction,
 } as const;
