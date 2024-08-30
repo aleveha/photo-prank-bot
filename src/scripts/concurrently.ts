da5b487d363a6049b2165344da2ca65bd76723a1
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-30 15:16:58 -0400

    refactor: add startup script instead of instrumentation hook

--- src/scripts/concurrently.ts
+++ src/scripts/concurrently.ts
@@ -1,10 +1,10 @@
 import { $ } from "bun";
 import { envs } from "~/configs/envs";
+import { startup } from "./startup";
 
 const DOMAIN = envs.APP_URL.split(".")[0].trim();
-
 if (!DOMAIN) {
 	throw new Error("Invalid APP_URL environment variable.");
 }
 
-await Promise.all([$`bunx next dev -p ${envs.PORT}`, $`bunx lt -s ${DOMAIN} -p ${envs.PORT}`]);
+await Promise.all([$`bunx next dev -p ${envs.PORT}`, $`bunx lt -s ${DOMAIN} -p ${envs.PORT}`, startup()]);
---
+++ src/scripts/startup.ts
@@ -0,0 +1,31 @@
+import { bot } from "~/bot";
+import { COMMANDS } from "~/bot/handlers/commands";
+import { envs } from "~/configs/envs";
+
+export async function startup() {
+	try {
+		await bot.api.setWebhook(`https://${envs.APP_URL}/api/bot`, {
+			allowed_updates: ["my_chat_member", "message"],
+			drop_pending_updates: true,
+		});
+	} catch (err) {
+		console.error("Failed to set webhook:", err);
+	}
+
+	try {
+		await bot.api.setMyCommands(COMMANDS);
+	} catch (err) {
+		console.error("Failed to set commands:", err);
+	}
+
+	if (!envs.isProduction) {
+		return;
+	}
+
+	try {
+		const version = envs.PULL_REQUEST_ID ? ` #${envs.PULL_REQUEST_ID}` : "";
+		await bot.api.sendMessage(envs.ADMIN_CHAT_ID, `Запущена новая версия${version}`);
+	} catch (err) {
+		console.error("Failed to send message:", err);
+	}
+}
