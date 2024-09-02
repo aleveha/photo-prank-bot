33c885af729f671c295c892d2ed65a884fbc403c
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-02 18:23:43 -0400

    feat: add privacy policy to bot

--- src/bot/handlers/commands/index.ts
+++ src/bot/handlers/commands/index.ts
@@ -5,4 +5,8 @@ export const COMMANDS: BotCommand[] = [
 		command: "start",
 		description: "Getting started! Obtain your unique links!",
 	},
+	{
+		command: "privacy",
+		description: "Learn more about our privacy policy!",
+	},
 ];
--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,6 +3,7 @@ import { Bot } from "grammy";
 import type { Context } from "~/bot/types";
 import { envs } from "~/configs/envs";
 import { i18n } from "./configs/i18n";
+import { privacyPolicy } from "./handlers/commands/privacy-policy";
 import { start } from "./handlers/commands/start";
 import { myChatMember } from "./handlers/events/my-chat-member";
 import { newChatMembers } from "./handlers/events/new-chat-members";
@@ -20,5 +21,6 @@ bot.on("my_chat_member", myChatMember);
 bot.on("message:new_chat_members", newChatMembers).use(ignoreGroupChats).use(verification);
 
 bot.command("start").use(rateLimiter).use(ignoreGroupChats).use(verification).use(start);
+bot.command("privacy").use(rateLimiter).use(ignoreGroupChats).use(verification).use(privacyPolicy);
 
 bot.errorBoundary((error) => console.error("An error occurred in the bot:", error));
