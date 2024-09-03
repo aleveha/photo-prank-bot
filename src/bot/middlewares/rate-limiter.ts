d83b3430b0ef0d4dad1ae0e38053cea352e58ef3
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-03 18:02:12 -0400

    feat: increase rate limiter time frame to 3 seconds

--- src/bot/middlewares/rate-limiter.ts
+++ src/bot/middlewares/rate-limiter.ts
@@ -1,3 +1,7 @@
 import { limit } from "@grammyjs/ratelimiter";
 
-export const rateLimiter = limit({ onLimitExceeded: (ctx) => console.warn("Rate limit exceeded", ctx.from?.id) });
+export const rateLimiter = limit({
+	limit: 1,
+	onLimitExceeded: (ctx) => console.warn("Rate limit exceeded", ctx.from?.id),
+	timeFrame: 3000,
+});
