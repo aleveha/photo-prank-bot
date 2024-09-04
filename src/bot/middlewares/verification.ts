5ac2b32a43b56be788bddb744a28d160537b5208
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-04 16:36:32 -0400

    fix: stop processing updates if verification fails, encapsulate chat adding functionality

--- src/bot/middlewares/verification.ts
+++ src/bot/middlewares/verification.ts
@@ -1,23 +1,15 @@
-import { sql } from "drizzle-orm";
 import type { NextFunction } from "grammy";
 import type { Context } from "~/bot/types";
-import { database, schema } from "~/configs/database";
-
-const insertChatQuery = database
-	.insert(schema.chat)
-	.values({ id: sql.placeholder("chatId") })
-	.onConflictDoNothing()
-	.prepare("insertChat");
+import { addChat } from "~/services/chat.service";
 
 export async function verification(ctx: Context, next: NextFunction) {
 	if (!ctx.chat) {
 		return;
 	}
 
-	try {
-		await insertChatQuery.execute({ chatId: ctx.chat.id });
-	} catch (error) {
-		console.error("Error inserting chat into database:", error);
+	const isChatAdded = await addChat(ctx.chat.id);
+	if (!isChatAdded) {
+		return;
 	}
 
 	await next();
