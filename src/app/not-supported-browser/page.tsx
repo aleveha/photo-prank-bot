9d8996c89eb00bf757a601b5a00bf43991018360
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-06 21:49:05 -0400

    feat: add translations for not supported browser page

--- src/app/not-supported-browser/page.tsx
+++ src/app/not-supported-browser/page.tsx
@@ -1,15 +1,19 @@
-export default async function Page() {
+import { useTranslations } from "next-intl";
+
+export default function Page() {
+	const t = useTranslations("not-supported-browser");
+
 	return (
 		<div className="flex flex-col gap-8 [&>p]:text-lg md:[&>p]:leading-loose md:text-center md:items-center">
-			<h1 className="text-3xl font-bold">Your browser is not supported</h1>
+			<h1 className="text-3xl font-bold">{t("title")}</h1>
 			<p>
-				If you open this page from in-app browser (e.g.&nbsp;Telegram):
-				<br />
-				<span className="underline underline-offset-4 font-semibold">
-					click three dots at the top right corner and choose "Open in ..." option.
-				</span>
+				{t.rich("content.tg-browser", {
+					ub: (content) => (
+						<span className="block underline underline-offset-4 font-semibold">{content}</span>
+					),
+				})}
 			</p>
-			<p>Otherwise, use another modern browser like Chrome, Firefox, or Safari.</p>
+			<p>{t("content.external-browser")}</p>
 		</div>
 	);
 }
