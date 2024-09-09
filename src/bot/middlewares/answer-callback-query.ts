5c2a9504bc27215657d71317faf5462be32d85bb
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-09 18:26:12 -0400

    fix: update answer-callback-query handler to handle old queries

--- src/bot/middlewares/answer-callback-query.ts
+++ src/bot/middlewares/answer-callback-query.ts
@@ -1,7 +1,20 @@
-import type { NextFunction } from "grammy";
+import { GrammyError, type NextFunction } from "grammy";
 import type { Context } from "~/bot/types";
 
 export async function answerCallbackQuery(ctx: Context, next: NextFunction) {
-	await ctx.answerCallbackQuery();
+	try {
+		await ctx.answerCallbackQuery();
+	} catch (err) {
+		if (!(err instanceof GrammyError)) {
+			throw err;
+		}
+
+		if (err.message.includes("query is too old") && !!ctx.msg?.reply_markup) {
+			await ctx.editMessageReplyMarkup(undefined);
+			return;
+		}
+
+		console.error(err);
+	}
 	await next();
 }
