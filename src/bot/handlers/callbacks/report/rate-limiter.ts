194b67f271a586d8d86cf9a17566e607dddec01a
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-11 15:04:20 -0400

    fix: notify user about spamming only when reports were actually send

--- src/bot/handlers/callbacks/report/rate-limiter.ts
+++ src/bot/handlers/callbacks/report/rate-limiter.ts
@@ -1,9 +1,16 @@
-import type { CallbackQueryContext } from "grammy";
+import type { CallbackQueryContext, NextFunction } from "grammy";
 import { DEFAULT_RATE_LIMITER_CONFIG } from "~/bot/middlewares/rate-limiter";
 import type { Context } from "~/bot/types";
+import { REPORT_VALUES } from "./constants";
 
-export async function reportCallbackRateLimitExceeded(ctx: CallbackQueryContext<Context>) {
+export async function reportCallbackRateLimitExceeded(ctx: CallbackQueryContext<Context>, next: NextFunction) {
 	DEFAULT_RATE_LIMITER_CONFIG.onLimitExceeded(ctx);
+
+	const callbackData = typeof ctx.match === "string" ? ctx.match : ctx.match[0];
+	if (!callbackData.includes(REPORT_VALUES.do)) {
+		await next();
+		return;
+	}
 
 	await ctx.reply(ctx.t("report-command.rate-limit-message"));
 }
