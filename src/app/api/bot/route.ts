4b045fc07cdd0ca057eb883f8b08e4f84ec7d05c
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-09-10 22:09:14 -0400

    fix: update error handling

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -5,4 +5,12 @@ export const preferredRegion = ["fra1", "cdg1", "lhr1"];
 import { webhookCallback } from "grammy";
 import { bot } from "~/bot";
 
-export const POST = webhookCallback(bot, "std/http");
+export async function POST(req: Request) {
+	try {
+		await webhookCallback(bot, "std/http")(req);
+	} catch (err) {
+		console.error("An error occurred in the bot:\n", err);
+	}
+
+	return new Response("Success!", { status: 200 });
+}
