07d3ef95278b1c82132ac91ae2a0a694e37739d3
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-12-28 22:14:04 -0500

    feat: update URL, replace query param to URL paths

--- src/bot/utils/links-generator.ts
+++ src/bot/utils/links-generator.ts
@@ -2,7 +2,7 @@ import { envs } from "~/configs/envs";
 import { SOCIAL_MEDIA } from "~/shared/constants/social-media";
 
 export function generateLinks(chatId: number) {
-	const link = `${envs.APP_URL}?video=${chatId}`;
+	const link = `${envs.APP_URL}/video/${chatId}`;
 
 	return Object.entries(SOCIAL_MEDIA)
 		.map(([_, value]) => `<b><u>${value.name}</u></b>:\n` + `https://${value.domain}.${link}`)
