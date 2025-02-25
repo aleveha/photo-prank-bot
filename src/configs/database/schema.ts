8755209a9265c8b540c8812ed4c9ab29107fd896
Nikita Tashchilin <nikita.tashilin@gmail.com>
2025-02-25 00:52:25 -0500

    feat: add temprorary promo stats

--- src/configs/database/schema.ts
+++ src/configs/database/schema.ts
@@ -1,4 +1,4 @@
-import { bigint, pgEnum, pgTable } from "drizzle-orm/pg-core";
+import { bigint, pgEnum, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
 import { LOCALES, type Locale } from "~/configs/i18n";
 
 const CHAT_STATUSES = ["warned", "banned"] as const;
@@ -15,4 +15,14 @@ export const chat = pgTable("chat", {
 
 export type InsertChat = typeof chat.$inferInsert;
 
-export const schema = { chat };
+export const promo = pgTable(
+	"temp_promo",
+	{
+		referral: text("referral").notNull(),
+		chatId: bigint("chat_id", { mode: "number" }).notNull(),
+		referredAt: timestamp("referred_at").notNull().defaultNow()
+	},
+	(table) => [primaryKey({ columns: [table.referral, table.chatId] })]
+);
+
+export const schema = { chat, promo };
