4dd25b1137529967b8f90c57fef97b5d363ba493
aleveha <nikita.tashilin@gmail.com>
2025-12-27 14:57:00 -0500

    fix: update POST handler in bot API to use Next.js response and error handling

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -3,14 +3,13 @@ export const fetchCache = "force-no-store";
 export const preferredRegion = ["fra1", "cdg1", "lhr1"];
 
 import { webhookCallback } from "grammy";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { bot } from "~/bot";
 
-export async function POST(req: Request) {
-	try {
-		await webhookCallback(bot, "std/http")(req);
-	} catch (err) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+	webhookCallback(bot, "next-js")(req, res).catch((err) => {
 		console.error("An error occurred in the bot:\n", err);
-	}
+	});
 
-	return new Response("Success!", { status: 200 });
+	return res.status(200).json({ message: "Success!" });
 }
