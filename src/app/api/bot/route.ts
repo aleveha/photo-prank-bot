fdcb91399171c0620d1ad686fa615acd58391bcf
aleveha <nikita.tashilin@gmail.com>
2026-01-11 11:03:36 -0500

    refactor: extract bot webhook handler into a separate variable outside of the POST request handler

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -2,9 +2,11 @@ import { webhookCallback } from "grammy";
 import type { NextRequest } from "next/server";
 import { bot } from "~/bot";
 
+const handleRequest = webhookCallback(bot, "std/http");
+
 export async function POST(req: NextRequest) {
 	try {
-		await webhookCallback(bot, "std/http")(req);
+		await handleRequest(req);
 	} catch (err) {
 		console.error("An error occurred in the bot:\n", err);
 	}
